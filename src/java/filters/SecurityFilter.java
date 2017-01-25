71632600c8d9adf380fc2920c0da67cd9553a1c9
barovig <semargl1349@gmail.com>
2017-01-25 19:03:09 +0000

    setup filtering mechanism

--- src/java/filters/SecurityFilter.java
+++ src/java/filters/SecurityFilter.java
@@ -6,12 +6,18 @@
 package filters;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
 import javax.servlet.Filter;
 import java.util.logging.LogRecord;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
@@ -29,6 +35,42 @@ public class SecurityFilter implements Filter{
 
     private FilterConfig filterConfig = null;
 	
+	private static enum Types{
+		INTEGER, DOUBLE, STRING, DATE
+	}
+	private String msg;
+	
+	private static final HashMap<String, Types> attribs;
+	static{
+		attribs = new HashMap<>();
+		attribs.put("id", Types.INTEGER);
+		attribs.put("action", Types.STRING);
+		attribs.put("listingNum", Types.INTEGER);
+		attribs.put("dateAdded", Types.DATE);
+		attribs.put("street", Types.STRING);
+		attribs.put("city", Types.STRING);
+		attribs.put("price", Types.DOUBLE);
+		attribs.put("typeId", Types.INTEGER);
+		attribs.put("styleId", Types.INTEGER);
+		attribs.put("bedrooms", Types.INTEGER);
+		attribs.put("bathrooms", Types.DOUBLE);
+		attribs.put("garageId", Types.INTEGER);
+		attribs.put("garageSize", Types.INTEGER);
+		attribs.put("lotSize", Types.STRING);
+		attribs.put("berRating", Types.STRING);		
+	}
+	
+	private static final HashMap<String, String> rgxMap;
+	static{
+		// regex matching invalid characters for parameters
+		rgxMap = new HashMap<>();
+		rgxMap.put("action", "[^a-z_]");
+		rgxMap.put("street", "[^-a-zA-Z0-9().]");
+		rgxMap.put("city", "[^-a-zA-Z0-9().]");
+		rgxMap.put("lotSize", "[^0-9x]");
+		rgxMap.put("berRating", "[^0-9A-G]");
+	}
+	
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException{
 		
@@ -36,14 +78,14 @@ public class SecurityFilter implements Filter{
 		HttpServletResponse httpResp = (HttpServletResponse)response;
 		
 		ServletContext sc = filterConfig.getServletContext();
-		String filterName = filterConfig.getFilterName();
-		
-		String username = httpReq.getParameter("j_username");
+						
+		// validate possible parameters
+		boolean validated = ValidateParameters(request);
 
-		String password = httpReq.getParameter("j_password");
-		ArrayList<String> attributes = Collections.list(request.getAttributeNames());
-		
-		attributes.isEmpty();
+		if(!validated){
+			httpResp.sendError(418, msg);
+			return;
+		}
 		filterConfig.getServletContext()
 				.log("Servlet path: "+httpReq.getServletPath());
 		chain.doFilter(request, response);
@@ -59,5 +101,71 @@ public class SecurityFilter implements Filter{
 		filterConfig= null;
 	}
 	
+	private boolean ValidateParameters(ServletRequest request) {
+		// get parameter names
+		List<String> names = Collections.list(request.getParameterNames());
+		// if param should be sanitised, process it using one of validators
+		boolean valid = true;
+		for(String p : names){
+			if(attribs.containsKey(p)){
+				switch(attribs.get(p)){
+					case INTEGER:
+						valid = validateInt(request.getParameter(p));
+						break;
+					case DOUBLE:
+						valid = validateDecimal(request.getParameter(p));
+						break;
+					case STRING:
+						valid = validateString(request.getParameter(p), rgxMap.get(p));
+						break;
+					case DATE:
+						valid = validateDate(request.getParameter(p));
+						break;
+					default:
+				}
+			}
+			if(!valid) break;
+		}
+		return valid;
+	}
+		
+	private boolean validateInt(String value){
+		try{
+			Integer.parseInt(value);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
 	
+	private boolean validateDecimal(String value){
+		try{
+			Double.parseDouble(value);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
+	
+	private boolean validateString(String value, String regex){
+		// regex should match for disallowed characters
+		Pattern r = Pattern.compile(regex);
+		Matcher m = r.matcher(value);
+		// return inverse of matching 
+		// (e.g. regex for special chars->matched->return false
+		return !m.find();
+	}
+	
+	private boolean validateDate(String date) {
+		DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		try{
+			Date d = format.parse(date);
+		}
+		catch(Exception e){
+			return false;
+		}
+		return true;
+	}
 }
